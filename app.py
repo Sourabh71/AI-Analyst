3577c417b89d65d5983e224fab246d3203cf3979
Sourabh71 <saurabh.270505@gmail.com>
2025-07-18 12:28:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if uploaded_file:
     if st.button("Run LLM Extraction"):
         with st.spinner("Calling AI..."):
 
-            OPENROUTER_API_KEY = "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"
+            OPENROUTER_API_KEY = ""
 
             prompt = f"""
 Read the following financial press release or statement and extract the key financial values:
